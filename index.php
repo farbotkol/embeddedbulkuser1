c9c5b3d5a1612e7a1addad0a354a44e8534d5f14
LFARBOTKOHEALTH <luke.farbotko@health.vic.gov.au>
2021-06-22 21:17:59 +1000

    e

--- index.php
+++ index.php
@@ -22,7 +22,7 @@
     <meta name="salesforce-login-handler" content="onLogin">
     <meta name="salesforce-logout-handler" content="onLogout">
     <meta name="salesforce-mask-redirects" content="<?php echo getenv('SALESFORCE_MASK_REDIRECTS');?>">
-    <meta name="salesforce-server-starturl" content="https://embeddedlogin-dev-ed.my.site.com/demo/services/oauth2/authorize?response_type=token& client_id=3MVG9Iu66FKeHhIPrRneLTDFdiuLfgLjycFpg6SbLpZAJScEXuD.oRdaWnJE7QGNFWHxunp0ut1& redirect_uri=https%3A%2F%2Fembeddedlogin.herokuapp.com%2F_callback.html&state=%2F">
+    <meta name="salesforce-server-starturl" content="https://bulkdemo-developer-edition.ap24.force.com/ExternalID/s/login&startURL=%2Fservices%2Foauth2%2Fauthorize%3Fresponse_type%3Dtoken%26client_id%3D3MVG9fe4g9fhX0E4jdnRdSUz8m.3eEkphBViVYtfDs4WLyh6usaAt4kBkbo0eldATsFgQ1BirAV0enelqUbT5%26redirect_uri%3Dhttps%253A%252F%252Fembeddedbulkuser1.herokuapp.com%252F_callback.php%26state%3Dhttps%253A%252F%252Fembeddedbulkuser1.herokuapp.com%252F">
 	<link href="https://<?php echo getenv('SALESFORCE_COMMUNITY_URL');?>/servlet/servlet.loginwidgetcontroller?type=css" rel="stylesheet" type="text/css" />
     <script src="https://<?php echo getenv('SALESFORCE_COMMUNITY_URL');?>/servlet/servlet.loginwidgetcontroller?type=javascript_widget" async defer></script>
   </head>
